e7c234bd96369dd4cfa22c5e73331c7394c4f3f2
Christophe Henry <christophe.j.henry@googlemail.com>
2011-03-10 22:47:14 +0000

    added missing copyright
    [SVN r69837]

--- example/lambda.cpp
+++ example/lambda.cpp
@@ -1,3 +1,7 @@
+// Copyright 2011 Christophe Henry
+// henry UNDERSCORE christophe AT hotmail DOT com
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
 #include <algorithm>
