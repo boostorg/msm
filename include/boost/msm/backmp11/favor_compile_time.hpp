ab9659a9718f759cc296cb5ba951b9fdbca41b3d
Christian Granzin <christian.granzin@gmail.com>
2025-03-30 12:22:20 -0400

    use simplified event filtering in rows

--- include/boost/msm/backmp11/favor_compile_time.hpp
+++ include/boost/msm/backmp11/favor_compile_time.hpp
@@ -127,47 +127,71 @@ struct cell_initializer<favor_compile_time>
 };
 
 // split the stt into rows grouped by event
+// template<typename Stt>
+// struct group_rows_by_event
+// {
+//     typedef typename generate_event_set<Stt>::event_set_mp11 event_set_mp11;
+//     // Consider only real rows
+//     template<typename Transition>
+//     using is_real_row = mp11::mp_not<typename has_not_real_row_tag<Transition>::type>;
+//     typedef mp11::mp_copy_if<Stt, is_real_row> real_rows;
+//     // Go through each row and group transitions by event.
+//     template<typename Row, typename Event>
+//     using has_event = std::is_same<typename Row::transition_event, Event>;
+//     template<typename V, typename Event>
+//     using partition_by_event = mp11::mp_list<
+//         // From first element we remove the rows that we have grouped
+//         mp11::mp_remove_if_q<
+//             mp11::mp_first<V>,
+//             mp11::mp_bind_back<has_event, Event>
+//             >,
+//         // To second element we append a group of rows that is handled by the event
+//         mp11::mp_push_back<
+//             mp11::mp_second<V>,
+//             mp11::mp_copy_if_q<
+//                 mp11::mp_first<V>,
+//                 mp11::mp_bind_back<has_event, Event>
+//                 >
+//             >
+//         >;
+//     typedef mp11::mp_fold<
+//         event_set_mp11,
+//         mp11::mp_list<real_rows, mp11::mp_list<>>,
+//         partition_by_event
+//         > grouped_rows;
+//     static_assert(mp11::mp_empty<mp11::mp_first<grouped_rows>>::value, "Internal error: Not all rows grouped");
+//     typedef mp11::mp_second<grouped_rows> type;
+// };
+
+// template<typename Stt, typename Event>
+// using get_rows_of_event_t = mp11::mp_at_c<
+//     typename group_rows_by_event<Stt>::type,
+//     get_event_id<Stt, Event>::value
+//     >;
+
 template<typename Stt>
-struct group_rows_by_event
+struct get_real_rows
 {
-    typedef typename generate_event_set<Stt>::event_set_mp11 event_set_mp11;
-    // Consider only real rows
     template<typename Transition>
     using is_real_row = mp11::mp_not<typename has_not_real_row_tag<Transition>::type>;
-    typedef mp11::mp_copy_if<Stt, is_real_row> real_rows;
-    // Go through each row and group transitions by event.
-    template<typename Row, typename Event>
-    using has_event = std::is_same<typename Row::transition_event, Event>;
-    template<typename V, typename Event>
-    using partition_by_event = mp11::mp_list<
-        // From first element we remove the rows that we have grouped
-        mp11::mp_remove_if_q<
-            mp11::mp_first<V>,
-            mp11::mp_bind_back<has_event, Event>
-            >,
-        // To second element we append a group of rows that is handled by the event
-        mp11::mp_push_back<
-            mp11::mp_second<V>,
-            mp11::mp_copy_if_q<
-                mp11::mp_first<V>,
-                mp11::mp_bind_back<has_event, Event>
-                >
-            >
-        >;
-    typedef mp11::mp_fold<
-        event_set_mp11,
-        mp11::mp_list<real_rows, mp11::mp_list<>>,
-        partition_by_event
-        > grouped_rows;
-    static_assert(mp11::mp_empty<mp11::mp_first<grouped_rows>>::value, "Internal error: Not all rows grouped");
-    typedef mp11::mp_second<grouped_rows> type;
+    typedef mp11::mp_copy_if<Stt, is_real_row> type;
 };
 
 template<typename Stt, typename Event>
-using get_rows_of_event = mp11::mp_at_c<
-    typename group_rows_by_event<Stt>::type,
-    get_event_id<Stt, Event>::value
-    >;
+struct get_rows_of_event
+{
+    template<typename Row>
+    using has_event = std::is_same<typename Row::transition_event, Event>;    
+    using type = mp11::mp_copy_if<
+        typename get_real_rows<Stt>::type,
+        has_event
+        >;
+};
+template<typename Stt, typename Event>
+using get_rows_of_event_t = typename get_rows_of_event<Stt, Event>::type;
+
+
+
 
 // Generates a singleton runtime lookup table that maps current state
 // to a function that makes the SM take its transition on the given
@@ -249,7 +273,7 @@ struct dispatch_table < Fsm, Stt, Event, ::boost::msm::back::favor_compile_time>
         // Fill in cells for matching transitions
         typedef mp11::mp_transform<
             preprocess_row,
-            get_rows_of_event<Stt, Event>
+            get_rows_of_event_t<Stt, Event>
             > preprocessed_rows;
         cell_initializer::init(
             entries,
